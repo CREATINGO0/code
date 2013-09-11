f18f7caeb35ff61ecf50d96fc4eab4da51f29629
chadluo <i@yukir.net>
2013-09-11 17:46:35 +1000

    130911

--- cs2041/wk7/poet.pl
+++ cs2041/wk7/poet.pl
@@ -8,17 +8,24 @@ if (@ARGV == 0) {
     exit 1;
 }
 
+# for each poem?.txt
+#   (for each line)
+#   for each word
+#     sum log_p for each poet
+#   sort poets by log_p
+#   return the one with top log_p
+
 foreach my $arg (@ARGV) {
 
-# for each word of poem?.txt calculate the log
-# and sum all.
+    my %log_p = ();
 
     open(UNK, "<$arg") or die "Cannot open file: $arg";
     while(<UNK>) {
 
+        my $sumlog = 0;
+
         $_ =~ tr/A-Z/a-z/;
         foreach my $toMatch (split(/[^A-Za-z]+/, $_)) {
-            print $toMatch."\n";
 
             foreach my $file (glob "poets/*.txt") {
 
@@ -37,14 +44,17 @@ foreach my $arg (@ARGV) {
                     }
                 }
                 my $log = log(++$matchcount/$wordcount);
+                $sumlog += $log;
                 $matchcount--;
                 $file =~ s/poets\///g;
                 $file =~ s/\.txt//g;
                 $file =~ s/_/ /g;
-                printf "log((%d+1)/%6d) = %8.4f %s\n", $matchcount, $wordcount, $log, $file;
+                $log_p{"$file"}=$sumlog;
 
             }
         }
+        print %log_p;
+
     }
 
 }
