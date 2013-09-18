48bc3ccd9753eece63009bf6f44699052c95c4c4
chadluo <i@yukir.net>
2013-09-18 18:02:45 +1000

    ./courses.pl COMP | tr "\n" " " | xargs ./lectures.pl -t

--- cs2041/wk8/lectures.pl
+++ cs2041/wk8/lectures.pl
@@ -4,6 +4,8 @@
 # use warnings;
 
 my $printMode = 0; # normal
+my %t_time = ();
+my @weekday = ("Mon","Tue","Wed","Thu","Fri");
 
 foreach my $arg (@ARGV) {
     if ($arg eq '-d') {
@@ -13,86 +15,51 @@ foreach my $arg (@ARGV) {
     }
 }
 
-if ($printMode != 2) {
+foreach my $course (@ARGV) {
 
-    foreach my $course (@ARGV) {
+    $course = uc $course;
+    my $url = "http://www.timetable.unsw.edu.au/2013/".$course.".html";
+    open F, "wget -q -O- $url|" or die "failed to retrieve courses";
 
-        $course = uc $course;
-        my $url = "http://www.timetable.unsw.edu.au/2013/".$course.".html";
-        open F, "wget -q -O- $url|" or die "failed to retrieve courses";
+    while (my $line=<F>) {
 
-        while (my $line=<F>) {
+        if ($line =~ /Lecture<\/a>/) {
+            $line=<F>;
 
-            if ($line =~ /Lecture<\/a>/) {
-                $line=<F>;
-
-                $line =~ /[^0-9]*T([12])<\/a/;
-                my $sem = "S".$1;
-                if ($printMode == 0) { # normal
-                    print "$course: $sem";
-                }
-
-                for (my $i=0;$i<5;$i++) {
-                    $line=<F>;
-                }
-
-                $line =~ s/<\/td>$//g;
-                $line =~ s/^\s*<.*>//g;
-                if ($printMode == 0) { # normal
-                    print " $line";
-                } elsif ($printMode == 1) {
-                    foreach my $slot (split(/,\s+/,$line)) {
-                        $slot =~ /([A-Za-z]{3})[^0-9]*([0-9]{2}):[0-9]{2}[\s\-]+([0-9]{2})/;
-                        my ($week,$start,$end) = ($1,$2,$3);
-                        while ($start < $end) {
-                            print "$sem $course $week $start\n";
-                            $start++;
-                        }
-                    }
-                }
+            $line =~ /[^0-9]*T([12])<\/a/;
+            my $sem = "S".$1;
+            if ($printMode == 0) { # normal
+                print "$course: $sem";
             }
-        }
 
-    }
-}
-
-if ($printMode == 2) { # table
-
-    my %t_time = ();
-    my @weekday = ("Mon","Tue","Wed","Thu","Fri");
-
-    foreach my $course (@ARGV) {
-
-        $course = uc $course;
-        my $url = "http://www.timetable.unsw.edu.au/2013/".$course.".html";
-        open F, "wget -q -O- $url|" or die "failed to retrieve courses";
-
-        while (my $line=<F>) {
-
-            if ($line =~ /Lecture<\/a>/) {
+            for (my $i=0;$i<5;$i++) {
                 $line=<F>;
+            }
 
-                $line =~ /[^0-9]*T([12])<\/a/;
-                my $sem = "S".$1;
-
-                for (my $i=0;$i<5;$i++) {
-                    $line=<F>;
-                }
-
-                $line =~ s/<\/td>$//g;
-                $line =~ s/^\s*<.*>//g;
+            $line =~ s/<\/td>$//g;
+            $line =~ s/^\s*<.*>//g;
+            if ($printMode == 0) { # normal
+                print " $line";
+            } else {
                 foreach my $slot (split(/,\s+/,$line)) {
                     $slot =~ /([A-Za-z]{3})[^0-9]*([0-9]{2}):[0-9]{2}[\s\-]+([0-9]{2})/;
                     my ($week,$start,$end) = ($1,$2,$3);
                     while ($start < $end) {
-                        $t_time{"$sem"}{"$week"}{"$start"}++;
+                        if ($printMode == 1) {
+                            print "$sem $course $week $start\n";
+                        } elsif ($printMode == 2) {
+                            $t_time{"$sem"}{"$week"}{"$start"}++;
+                        }
                         $start++;
                     }
                 }
             }
         }
-
     }
+
+}
+
+if ($printMode == 2) {
 
     for (my $i=1;$i<3;$i++) {
 
@@ -110,4 +77,5 @@ if ($printMode == 2) { # table
         }
 
     }
+
 }
