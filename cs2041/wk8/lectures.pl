45077f109ed3073476db671148fb672a308f8403
chadluo <i@yukir.net>
2013-09-18 17:55:50 +1000

    wk8 done w/o opti

--- cs2041/wk8/lectures.pl
+++ cs2041/wk8/lectures.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
+# use strict;
+# use warnings;
 
 my $printMode = 0; # normal
 
@@ -22,16 +22,20 @@ if ($printMode != 2) {
         open F, "wget -q -O- $url|" or die "failed to retrieve courses";
 
         while (my $line=<F>) {
+
             if ($line =~ /Lecture<\/a>/) {
                 $line=<F>;
+
                 $line =~ /[^0-9]*T([12])<\/a/;
                 my $sem = "S".$1;
                 if ($printMode == 0) { # normal
                     print "$course: $sem";
                 }
+
                 for (my $i=0;$i<5;$i++) {
                     $line=<F>;
                 }
+
                 $line =~ s/<\/td>$//g;
                 $line =~ s/^\s*<.*>//g;
                 if ($printMode == 0) { # normal
@@ -53,12 +57,57 @@ if ($printMode != 2) {
 }
 
 if ($printMode == 2) { # table
-    print "S1       Mon   Tue   Wed   Thu   Fri\n";
-    for (my $i=9;$i<21;$i++) {
-        printf "%2d:00\n",$i
+
+    my %t_time = ();
+    my @weekday = ("Mon","Tue","Wed","Thu","Fri");
+
+    foreach my $course (@ARGV) {
+
+        $course = uc $course;
+        my $url = "http://www.timetable.unsw.edu.au/2013/".$course.".html";
+        open F, "wget -q -O- $url|" or die "failed to retrieve courses";
+
+        while (my $line=<F>) {
+
+            if ($line =~ /Lecture<\/a>/) {
+                $line=<F>;
+
+                $line =~ /[^0-9]*T([12])<\/a/;
+                my $sem = "S".$1;
+
+                for (my $i=0;$i<5;$i++) {
+                    $line=<F>;
+                }
+
+                $line =~ s/<\/td>$//g;
+                $line =~ s/^\s*<.*>//g;
+                foreach my $slot (split(/,\s+/,$line)) {
+                    $slot =~ /([A-Za-z]{3})[^0-9]*([0-9]{2}):[0-9]{2}[\s\-]+([0-9]{2})/;
+                    my ($week,$start,$end) = ($1,$2,$3);
+                    while ($start < $end) {
+                        $t_time{"$sem"}{"$week"}{"$start"}++;
+                        $start++;
+                    }
+                }
+            }
+        }
+
     }
-    print "S2       Mon   Tue   Wed   Thu   Fri\n";
-    for (my $i=9;$i<21;$i++) {
-        printf "%2d:00\n",$i
+
+    for (my $i=1;$i<3;$i++) {
+
+        print "S$i       Mon   Tue   Wed   Thu   Fri\n";
+        for (my $j=9;$j<21;$j++) {
+            printf "%02d:00",$j;
+            foreach my $day (@weekday) {
+                if ($t_time{"S$i"}{"$day"}{"$j"} != 0) {
+                    printf "%6d",$t_time{"S$i"}{"$day"}{"$j"};
+                } else {
+                    print "      ";
+                }
+            }
+            printf "\n";
+        }
+
     }
 }
