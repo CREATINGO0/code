69393d43e631e1e9c41fe05a41e0d5fc38596daf
chadluo <i@yukir.net>
2013-09-22 07:07:15 +1000

    indentation

--- cs2041/ass1/perl2python.pl
+++ cs2041/ass1/perl2python.pl
@@ -3,14 +3,16 @@
 use strict;
 use warnings;
 
-my $indent = 0;
+my $g_indent = 0;
 
 while (my $line = <>) {
 
-    # indentation
+    # rip off original indentations
+    $line =~ s/^\s*//g;
 
-    if ($line =~ /{\s*$/) {
-        $indent++;
+    if ($line =~ /^\s*}\s*$/) {
+        # meets closing bracket indicating reducing indentation
+        $g_indent--;
     }
 
     if ($line =~ /^#!/ && $. == 1) {
@@ -20,19 +22,37 @@ while (my $line = <>) {
 
     } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
         # Blank & comment lines can be passed unchanged
+        &printIndent();
 
         print $line;
 
     } elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
         # Python's print adds a new-line character by default
         # so we need to delete it from the Perl print statement
+        &printIndent();
 
         print "print \"$1\"\n";
 
     } else {
         # Lines we can't translate are turned into comments
+        &printIndent();
 
         print "#$line\n";
 
+    }
+
+    if ($line =~ /{\s*$/) {
+        # handle increase indent finally so that won't indent
+        # lines with {
+        $g_indent++;
+    }
+
+
+}
+
+sub printIndent {
+    # 4 spaces not tabs
+    for(my $i=$g_indent;$i>0;$i--) {
+        print "    ";
     }
 }
